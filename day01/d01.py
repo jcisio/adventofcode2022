0abcb6c731621765743eff26617ad8e2a6d36fef
Hai-Nam Nguyen <han.nguyen@criteo.com>
2022-12-01 22:04:25 +0100

    Day 01 Part 1

--- day01/d01.py
+++ day01/d01.py
@@ -2,9 +2,18 @@ f = open(__file__[:-3] + '.in', 'r')
 
 
 def solve(lines):
-    return 1
+    elves = []
+    current_elf = 0
+    lines.append(0)
+    for calorie in lines:
+        if calorie == '':
+            elves.append(current_elf)
+            current_elf = 0
+        else:
+            current_elf += int(calorie)
+    return max(elves)
 
 
-lines = f.read().strip().split('\n'))
+lines = f.read().strip().split('\n')
 print("Puzzle 1: ", solve(lines))
-print("Puzzle 2: ", solve(lines))
+#print("Puzzle 2: ", solve(lines))
