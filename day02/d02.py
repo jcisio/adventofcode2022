7ad870237a8ed103ae896ae0048e406cc9b859b6
Hai-Nam Nguyen <han.nguyen@criteo.com>
2022-12-02 08:48:55 +0100

    Day 02 Part 2

--- day02/d02.py
+++ day02/d02.py
@@ -1,13 +1,13 @@
 f = open(__file__[:-3] + '.in', 'r')
 
 
-def calculate_score(lines, points):
+def calculate_score1(lines, points):
     score = 0
     for play in lines:
         x,y = play.split()
         # Your shape score
         score += points[y]
-        # Your result score
+        # Your round score
         if points[x] == points[y]:
             score += 3
         elif points[x] == 1:
@@ -19,7 +19,26 @@ def calculate_score(lines, points):
     return score
 
 
+def calculate_score2(lines, points):
+    score = 0
+    for play in lines:
+        x,y = play.split()
+        # Your round score
+        score += points[y]
+        # Your shape score
+        if points[y] == 3:
+            score += points[x]
+        elif x == 'A':
+            score += points['C'] if y == 'X' else points['B']
+        elif x == 'B':
+            score += points['A'] if y == 'X' else points['C']
+        elif x == 'C':
+            score += points['B'] if y == 'X' else points['A']
+    return score
+
+
 lines = f.read().strip().split('\n')
 points = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
-score = calculate_score(lines, points)
-print("Puzzle 1: ", score)
+print("Puzzle 1: ", calculate_score1(lines, points))
+points = {'A': 1, 'B': 2, 'C': 3, 'X': 0, 'Y': 3, 'Z': 6}
+print("Puzzle 2: ", calculate_score2(lines, points))
