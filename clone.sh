062ca77562d13789136b9e1e7c97023f30eba566
Hai-Nam Nguyen <han.nguyen@criteo.com>
2022-12-02 08:58:43 +0100

    Fix clone.sh to work on Mac OS X

--- clone.sh
+++ clone.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cp -r day01 day$1
 cd day$1
-rename s/01/$1/ *.*
-curl "https://adventofcode.com/2022/day/$1/input" -H "cookie: session=$AOC2022SID" > d$1.in
-echo -n > problem.md
+mv d01.py d$1.py
+mv d01.in d$1.in
+day=$(echo $1 | sed 's/^0//')
+curl "https://adventofcode.com/2022/day/${day}/input" -s -H "cookie: session=$AOC2022SID" > d$1.in
+echo > problem.md
 git add d$1.in problem.md
