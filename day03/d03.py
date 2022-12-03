f92e9ae3af03868985ee5709c9ff9aa657db2ec4
Hai-Nam Nguyen <han.nguyen@criteo.com>
2022-12-03 06:29:49 +0100

    Day 03 Part 2

--- day03/d03.py
+++ day03/d03.py
@@ -5,8 +5,7 @@ def get_score(c):
     s = ''.join([chr(i+ord('a')) for i in range(26)] + [chr(i+ord('A')) for i in range(26)])
     return s.index(c) + 1
 
-
-def solve(lines):
+def solve1(lines):
     score = 0
     for line in lines:
         half = len(line)//2
@@ -16,7 +15,16 @@ def solve(lines):
                 break
     return score
 
+def solve2(lines):
+    score = 0
+    for i in range(len(lines)//3):
+        for c in lines[i*3]:
+            if c in lines[i*3+1] and c in lines[i*3+2]:
+                score += get_score(c)
+                break
+    return score
+
 
 lines = f.read().strip().split('\n')
-priorities = solve(lines)
-print("Puzzle 1: ", priorities)
+print("Puzzle 1: ", solve1(lines))
+print("Puzzle 2: ", solve2(lines))
