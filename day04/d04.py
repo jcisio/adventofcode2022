16e394c87ed85226f6402f4e8b38df42c6372587
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-04 06:42:39 +0100

    Day 04 Part 2

--- day04/d04.py
+++ day04/d04.py
@@ -2,9 +2,16 @@ f = open(__file__[:-3] + '.in', 'r')
 
 
 def solve(pairs):
-    return sum((p[0]>=p[2] and p[1]<=p[3]) or (p[0]<=p[2] and p[1]>=p[3]) for p in pairs)
+    return sum(p[0]==p[2] or p[1]>=p[3] for p in pairs)
+
+
+def solve2(pairs):
+    return sum(p[0]==p[2] or p[2]<=p[1] for p in pairs)
 
 
 lines = f.read().strip().split('\n')
 pairs = [list(map(int, line.replace(',','-').split('-'))) for line in lines]
+# Ordering.
+pairs = [p if p[0]<p[2] else [p[2],p[3],p[0],p[1]] for p in pairs]
 print("Puzzle 1: ", solve(pairs))
+print("Puzzle 2: ", solve2(pairs))
