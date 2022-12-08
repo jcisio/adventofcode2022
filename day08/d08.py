809e35db7fa0462d351d1b22f15dc82b7403ce2b
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-08 06:56:06 +0100

    Day 08 Clean up

--- day08/d08.py
+++ day08/d08.py
@@ -1,7 +1,7 @@
 f = open(__file__[:-3] + '.in', 'r')
 
 
-def solve(trees):
+def solve(trees, next):
     # visibility from top, bottom, left, right
     visibility = [[[True, True, True, True] for tree in row] for row in trees]
     # it seems that it's a square
@@ -11,19 +11,15 @@ def solve(trees):
         for i in range(1, L-1):
             if d == 0:
                 current = (0, i)
-                next = (1, 0)
             elif d == 1:
                 current = (L-1, i)
-                next = (-1, 0)
             elif d == 2:
                 current = (i, 0)
-                next = (0, 1)
             else:
                 current = (i, L-1)
-                next = (0, -1)
             min = trees[current[0]][current[1]]
             for j in range(1, L-1):
-                current = (current[0] + next[0], current[1] + next[1])
+                current = (current[0] + next[d][0], current[1] + next[d][1])
                 if trees[current[0]][current[1]] <= min:
                     visibility[current[0]][current[1]][d] = False
                 else:
@@ -31,7 +27,7 @@ def solve(trees):
     return sum([sum([(1 if any(visibility[i][j]) else 0) for j in range(L)]) for i in range(L)])
 
 
-def solve2(trees):
+def solve2(trees, next):
     L = len(trees)
     maxv = 0
     for j in range(1, L-1):
@@ -39,27 +35,20 @@ def solve2(trees):
             h = trees[j][i]
             v = 1
             for d in range(4):
-                if d == 0:
-                    next = (1, 0)
-                elif d == 1:
-                    next = (-1, 0)
-                elif d == 2:
-                    next = (0, 1)
-                else:
-                    next = (0, -1)
                 distance = 0
                 for k in range(1, L-1):
-                    c = [j - next[0]*k, i - next[1]*k]
+                    c = [j - next[d][0]*k, i - next[d][1]*k]
                     if min(c) < 0 or max(c) > L-1:
                         break
                     distance += 1
                     if trees[c[0]][c[1]] >= h:
                         break
-                print(j, i, d, distance)
                 v = v * distance
             maxv = max(maxv, v)
     return maxv
 
+# It would be better to use 1D array with tuple as index for easier manipulation.
 trees = [list(map(int, line)) for line in f.read().strip().split('\n')]
-print("Puzzle 1: ", solve(trees))
-print("Puzzle 2: ", solve2(trees))
+next = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+print("Puzzle 1: ", solve(trees, next))
+print("Puzzle 2: ", solve2(trees, next))
