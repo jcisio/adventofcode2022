0a3e29a41cb0f80c2bda508ab1eb0165a079b897
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-08 06:50:48 +0100

    Day 08 Part 2

--- day08/d08.py
+++ day08/d08.py
@@ -4,10 +4,10 @@ f = open(__file__[:-3] + '.in', 'r')
 def solve(trees):
     # visibility from top, bottom, left, right
     visibility = [[[True, True, True, True] for tree in row] for row in trees]
+    # it seems that it's a square
+    L = len(trees)
     # direction
     for d in range(4):
-        # it seems that it's a square
-        L = len(trees)
         for i in range(1, L-1):
             if d == 0:
                 current = (0, i)
@@ -31,5 +31,35 @@ def solve(trees):
     return sum([sum([(1 if any(visibility[i][j]) else 0) for j in range(L)]) for i in range(L)])
 
 
+def solve2(trees):
+    L = len(trees)
+    maxv = 0
+    for j in range(1, L-1):
+        for i in range(1, L-1):
+            h = trees[j][i]
+            v = 1
+            for d in range(4):
+                if d == 0:
+                    next = (1, 0)
+                elif d == 1:
+                    next = (-1, 0)
+                elif d == 2:
+                    next = (0, 1)
+                else:
+                    next = (0, -1)
+                distance = 0
+                for k in range(1, L-1):
+                    c = [j - next[0]*k, i - next[1]*k]
+                    if min(c) < 0 or max(c) > L-1:
+                        break
+                    distance += 1
+                    if trees[c[0]][c[1]] >= h:
+                        break
+                print(j, i, d, distance)
+                v = v * distance
+            maxv = max(maxv, v)
+    return maxv
+
 trees = [list(map(int, line)) for line in f.read().strip().split('\n')]
 print("Puzzle 1: ", solve(trees))
+print("Puzzle 2: ", solve2(trees))
