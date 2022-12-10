2b5dcf80e479723a234e565490a3a1c25a29359d
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-10 06:59:45 +0100

    Day 10 Part 2

--- day10/d10.py
+++ day10/d10.py
@@ -1,25 +1,30 @@
 f = open(__file__[:-3] + '.in', 'r')
 
 
+def add_pixels(pixels, v, current, cycles):
+    for _ in range(cycles):
+        current = (current + 1) % 40
+        pixels.append('#' if v - 1 <= current - 2 <= v + 1 else '.')
+
+
 def solve(lines):
     start, steps, count = 20, 40, 6
     v, s, cycle, next = 1, 0, 1, start
-    print(cycle, v)
+    pixels = []
     for line in lines:
         ins = line.split()
-        if ins[0] == 'noop':
-            cycle += 1
-        else:
-            cycle += 2
+        cycles = 1 if ins[0] == 'noop' else 2
+        add_pixels(pixels, v, cycle, cycles)
+        cycle += cycles
         if cycle > next:
-#            print('OK', cycle, v)
             s += v * next
             next += steps
-            if next > start + steps*(count-1):
-                break
+        if next > steps * (count+1):
+            break
         if ins[0] == 'addx':
             v += int(ins[1])
-#        print(cycle, v)
+    for i in range(count):
+        print(''.join(pixels[i*40:(i+1)*40]))
     return s
 
 
