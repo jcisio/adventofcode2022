1bb4f50618771f547b3e505569f84b3b8affea33
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-11 07:30:36 +0100

    Day 11 Part 2

--- day11/d11.py
+++ day11/d11.py
@@ -2,6 +2,8 @@ f = open(__file__[:-3] + '.in', 'r')
 
 
 class Monkey:
+    magic_number = 0
+
     def __init__(self, items, op1, op2, test, target1, target2):
         self.items = items
         self.op = (op1, op2)
@@ -14,7 +16,7 @@ class Monkey:
             level = level + self.op[1]
         elif self.op[0] == '*':
             level = level * (self.op[1] if self.op[1] > 0 else level)
-        return level//3
+        return level//3 if self.magic_number == 0 else level % self.magic_number
 
     def inspect(self):
         targets = []
@@ -35,8 +37,6 @@ def do_round(monkeys):
         targets = m.inspect()
         for t in targets:
             monkeys[t[1]].items.append(t[0])
-#    for m in monkeys:
-#        print(m)
 
 
 def parse_data(lines):
@@ -55,13 +55,23 @@ def parse_data(lines):
     return monkeys
 
 
-def solve(monkeys):
-    for _ in range(20):
+def solve(monkeys, n=20):
+    for _ in range(n):
         do_round(monkeys)
+#    for m in monkeys: print(m)
     inspections = sorted(map(lambda x: x.inspections, monkeys))
     return inspections[-1]*inspections[-2]
 
 
+def solve2(monkeys):
+    Monkey.magic_number = 1
+    for m in monkeys:
+        Monkey.magic_number *= m.test
+    return solve(monkeys, 10000)
+
+
 lines = f.read().strip().split('\n')
-monkeys = parse_data(lines)
+monkeys = parse_data(lines.copy())
 print("Puzzle 1: ", solve(monkeys))
+monkeys = parse_data(lines)
+print("Puzzle 2: ", solve2(monkeys))
