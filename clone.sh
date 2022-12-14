e7653f87ae8dbbc86eb04a3b97a672a1e4f31ba1
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-14 07:20:38 +0100

    Not include input

--- clone.sh
+++ clone.sh
@@ -4,7 +4,8 @@ cd day$1
 mv d01.py d$1.py
 mv d01.in d$1.in
 day=$(echo $1 | sed 's/^0//')
-curl "https://adventofcode.com/2022/day/${day}/input" -s -H "cookie: session=$AOC2022SID" > d$1.in
+curl "https://adventofcode.com/2022/day/${day}/input" -s -H "cookie: session=$AOC2022SID" > d$1.test
+echo > d$1.in
 echo > problem.md
 git add d$1.in problem.md
 git commit -am "Day $1"
