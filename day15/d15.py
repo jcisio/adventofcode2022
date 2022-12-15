e36d271d73bb0ed208514d0357fa221e6a8d3d30
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-15 10:59:44 +0100

    Day 15 clean up

--- day15/d15.py
+++ day15/d15.py
@@ -12,8 +12,6 @@ class Problem:
         self.sensors = sensors
         for i in range(len(self.sensors)):
             self.sensors[i].append(self.distance(self.sensors[i]))
-            s = self.sensors[i]
-#            print(s[0], s[0]-s[4])
 
     @staticmethod
     def distance(sensor):
@@ -21,40 +19,26 @@ class Problem:
 
     def get_start_stop(self, n):
         l = []
-#        print(f"--- Line {n} ---")
         for s in self.sensors:
-#            print(f"sensor {s} distance {abs(s[1] - n)}")
             if abs(s[1] - n) <= s[4]:
                 d = s[4] - abs(s[1] - n)
                 l.append((s[0] - d, s[0] + d))
-#                print(f'Position of this sensor: {s[0] - d}, {s[0] + d}')
         return l
 
-
     def draw_line(self, n):
-        m = set()
-        b = set()
-        for s in self.sensors:
-            if s[3] == n:
-                b.add(s[2])
-            if abs(s[1] - n) < s[4]:
-                d = s[4] - abs(s[1] - n)
-                for i in range(s[0] - d, s[0] + d + 1):
-                    m.add(i)
-        #print(m, b)
-        return len(m) - len(b)
+        b = set([s[2] for s in self.sensors if s[3] == n])
+        m = functools.reduce((lambda x, y: x + y), [list(range(s[0], s[1] + 1)) for s in self.get_start_stop(n)])
+        return len(set(m)) - len(b)
 
     def find_bacon(self, start, stop):
-        #print(self.sensors)
         for y in range(start, stop + 1):
-            s = sorted(self.get_start_stop(y), key=lambda x:x[0])
+            s = sorted(self.get_start_stop(y), key=lambda x: x[0])
             i = start
             for p in s:
                 if p[1] <= i:
                     continue
-                if p[0] > i+1:
-                    print(i+1, y, s)
-                    return y+(i+1)*stop
+                if p[0] > i + 1:
+                    return y + (i + 1) * 4000000
                 if p[1] >= stop:
                     break
                 i = p[1]
@@ -63,23 +47,18 @@ class Problem:
 
 class Solver:
     def __init__(self, input) -> None:
-        self.input = input
-        self.problem = None
-        pass
-
-    def parse_input(self):
-        return [list(map(int, re.findall(r'-?\d+', x))) for x in self.input]
+        self.input = [list(map(int, re.findall(r'-?\d+', x))) for x in input]
 
     def solve(self, part=1):
-        self.problem = Problem(self.parse_input())
-        y, m = (10, 20) if len(self.problem.sensors) < 20 else (2000000,4000000)
-        if part==1:
-            return self.problem.draw_line(y)
+        problem = Problem(self.input)
+        y, m = (10, 20) if len(problem.sensors) < 20 else (2000000, 4000000)
+        if part == 1:
+            return problem.draw_line(y)
         else:
-            return self.problem.find_bacon(0, m)
+            return problem.find_bacon(0, m)
 
 
-f = open(__file__[:-3] + '.test', 'r')
+f = open(__file__[:-3] + '.in', 'r')
 solver = Solver(f.read().strip().split('\n'))
-#print("Puzzle 1: ", solver.solve())
+print("Puzzle 1: ", solver.solve())
 print("Puzzle 2: ", solver.solve(2))
