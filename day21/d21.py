3c1c8f75f48853de6e072c26a02d4903b7b581cd
Hai-Nam Nguyen <hainam@jcisio.com>
2022-12-21 07:13:55 +0100

    Day 21 Part 1 refactor

--- day21/d21.py
+++ day21/d21.py
@@ -3,52 +3,63 @@ Advent Of Code
 --- Day 21: Monkey Math ---
 https://adventofcode.com/2022/day/21
 """
-import functools
 import re
 
 
 class Problem:
-    def __init__(self, input) -> None:
-        self.printer = print if __debug__ else lambda *x: None
-        self.monkeys = input
+    class Monkey:
+        monkeys=dict()
+        def __init__(self, definition) -> None:
+            name, attr = definition.split(': ')
+            self.name = name
+            r = re.findall('[^\d ]+', attr)
+            if r:
+                self.op = r[1]
+                self.arg = (r[0], r[2])
+                self._val = None
+            else:
+                self._val = int(attr)
+            # Reverse dependencies
+            self.dep = set()
+            self.monkeys[name] = self
 
-    def calculate(self, monkey) -> int:
-        m = self.monkeys[monkey]
-        if 'val' not in m:
-            if m['arg'][1] == '+':
-                m['val'] = self.calculate(m['arg'][0]) + self.calculate(m['arg'][2])
-            elif m['arg'][1] == '-':
-                m['val'] = self.calculate(m['arg'][0]) - self.calculate(m['arg'][2])
-            elif m['arg'][1] == '*':
-                m['val'] = self.calculate(m['arg'][0]) * self.calculate(m['arg'][2])
-            elif m['arg'][1] == '/':
-                m['val'] = self.calculate(m['arg'][0]) / self.calculate(m['arg'][2])
+        @staticmethod
+        def get(name: str):
+            return Problem.Monkey.monkeys[name]
+
+        @property
+        def val(self):
+            if self._val != None:
+                return self._val
+            if self.op == '+':
+                self._val = self.get(self.arg[0]).val + self.get(self.arg[1]).val
+            elif self.op == '-':
+                self._val = self.get(self.arg[0]).val - self.get(self.arg[1]).val
+            elif self.op == '*':
+                self._val = self.get(self.arg[0]).val * self.get(self.arg[1]).val
+            elif self.op == '/':
+                self._val = self.get(self.arg[0]).val / self.get(self.arg[1]).val
             else:
                 raise ValueError
-        return m['val']
+            return self._val
+
+    def __init__(self) -> None:
+        self.printer = print if __debug__ else lambda *x: None
 
     def solve(self):
-        return self.calculate('root')
+        return int(Problem.Monkey.get('root').val)
 
 
 class Solver:
     def __init__(self, input) -> None:
-        monkeys = {}
-        for line in input:
-            name, attr = line.split(': ')
-            r = re.findall('[^\d ]+', attr)
-            if r:
-                monkeys[name] = {'arg': list(r)}
-            else:
-                monkeys[name] = {'val': int(attr)}
-        self.input = monkeys
+        self.input = [Problem.Monkey(line) for line in input]
 
     def solve(self, part=1):
-        problem = Problem(self.input)
-        return problem.solve()
+        problem = Problem()
+        return problem.solve() if part==1 else problem.solve2()
 
 
-f = open(__file__[:-3] + '.test', 'r')
+f = open(__file__[:-3] + '.in', 'r')
 solver = Solver(f.read().strip().split('\n'))
 print("Puzzle 1: ", solver.solve())
 #print("Puzzle 2: ", solver.solve(2))
